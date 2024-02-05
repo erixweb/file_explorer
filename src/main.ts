7a8d5857680159f454409ebb944d1a3a3bd3d043
erixweb <erikmorenodev@gmail.com>
2024-02-05 20:59:22 +0100

    Add top level await

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,17 @@ import { readDir, renameFile } from "@tauri-apps/api/fs"
 import { appDir, appFiles } from "./store"
 import "./styles.css"
 
-const readDirectory = await readDirAsync({ dir: "E:/" })
-appFiles.set(readDirectory)
+const toplevel = async () => {
+	const readDirectory = await readDirAsync({ dir: "E:/" })
+	appFiles.set(readDirectory)
+}
+toplevel()
 
 const app = new App({
 	target: document.getElementById("app")!,
 })
 
 export async function readDirAsync({ dir }: { dir: string }) {
-	console.log(dir)
 	return readDir(dir)
 }
 export async function renameFileAsync(initial: string, result: string) {
